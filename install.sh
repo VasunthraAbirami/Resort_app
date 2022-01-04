0ceb69414074f6a26c8bb634f98ce54bacba15d9
VasunthraAbirami <85115402+VasunthraAbirami@users.noreply.github.com>
2022-01-04 13:07:18 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-aws s3 cp s3://resort-bucket/resort.war/target/resort-application/resort.war /tmp
+aws s3 cp s3://resort-bucket/target/resort.war/resort-application/resort.war /tmp
 sudo mv /tmp/resort.war /opt/tomcat/webapps/resort.war
